389632a7d313c00732d02df562d3fb5463bc4e87
thanusree9246 <85348252+thanusree9246@users.noreply.github.com>
2021-06-11 17:21:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+seaima = lodeimage("sea.png")
+shipima1 = loadAnimation(//list the name of png files that you want to load);
+
+
+
+function preload(){
+
+}
+
+function setup(){
+  createCanvas(400,400);
+  spriteName.addImage (seaima);
+}
+
+function draw() {
+  background("blue");
+ if (sea.x < 0){
+sea.x = sea.width/2;
+
+
+ }
+}
